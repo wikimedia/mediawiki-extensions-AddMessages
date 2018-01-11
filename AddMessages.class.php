cb58e30c8ddb5f76e059d86cf731bea7e00ee91a
Umherirrender <umherirrender_de.wp@web.de>
2018-01-11 16:58:58 +0100

    Improve some parameter docs
    Change-Id: I3d704c26b005e93e6a4799b13a6582e366d28567

--- AddMessages.class.php
+++ AddMessages.class.php
@@ -12,9 +12,9 @@ class AddMessages {
 	 * or add a new message
 	 *
 	 * @global array $wgAmMessages
-	 * @param $cache
-	 * @param $code
-	 * @param array $alldata
+	 * @param LocalisationCache $cache
+	 * @param string $code
+	 * @param array &$alldata
 	 * @return bool
 	 */
 	public static function onLocalisationCacheRecache( $cache, $code, &$alldata ) {
