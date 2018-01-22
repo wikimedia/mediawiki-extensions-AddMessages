c8922cd0901ddc68b0e41c999c46558a49fb332b
Jayprakash12345 <0freerunning@gmail.com>
2018-01-22 08:54:09 +0000

    Convert AddMessages to use extension registration
    Bug: T185415
    Change-Id: I6317f7206850f169ed7695fb277a31ae2e42ec39

--- AddMessages.php
+++ AddMessages.php
@@ -1,28 +1,15 @@
 <?php
-# Alert the user that this is not a valid access point to MediaWiki if they
-# try to access the special pages file directly.
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo <<<EOT
-To install this extension, put the following line in LocalSettings.php:
-require_once( "\$IP/extensions/AddMessages/AddMessages.php" );
-EOT;
-	exit( 1 );
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'AddMessages' );
+	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	$wgMessagesDirs['AddMessages'] = __DIR__ . '/i18n';
+	wfWarn(
+		'Deprecated PHP entry point used for the AddMessages extension. ' .
+		'Please use wfLoadExtension instead, ' .
+		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+	);
+	return;
+} else {
+	die( 'This version of the AddMessages extension requires MediaWiki 1.29+' );
 }
-
-$wgExtensionCredits['other'][] = [
-	'path' => __FILE__,
-	'name' => 'AddMessages',
-	'author' => 'Ike Hecht',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:AddMessages',
-	'descriptionmsg' => 'addmessages-desc',
-	'version' => '0.2',
-];
-
-$wgAutoloadClasses['AddMessages'] = __DIR__ . '/AddMessages.class.php';
-$wgHooks['LocalisationCacheRecache'][] = 'AddMessages::onLocalisationCacheRecache';
-$wgMessagesDirs['AddMessages'] = __DIR__ . '/i18n';
-
-// Array of message key and value pairs. Will overwrite existing messages.
-// Set in LocalSettings.php
-// Example: $wgAmMessages = array( 'toolbox' => 'My Toolbox' );
-$wgAmMessages = [];
