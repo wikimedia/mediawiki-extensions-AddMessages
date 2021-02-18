4bce342640bcf3b3177950c97a548c2c7e7678d0
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2021-02-18 10:20:13 +0100

    Fix all PHPCS code style warnings
    This patch also removes the pointless `return true`. This is
    the default anyway for hook handlers and doesn't do anything.
    
    Change-Id: Ic17300a0f0a84f490bf8e1c79f4a7c7313c56e88

--- AddMessages.php
+++ AddMessages.php
@@ -11,17 +11,13 @@ class AddMessages {
 	 * Hook to replace default messages with customized user messages
 	 * or add a new message
 	 *
-	 * @global array $wgAmMessages
 	 * @param LocalisationCache $cache
 	 * @param string $code
 	 * @param array &$alldata
-	 * @return bool
 	 */
 	public static function onLocalisationCacheRecache( $cache, $code, &$alldata ) {
 		global $wgAmMessages;
 		$alldata['messages'] = array_merge( $alldata['messages'], $wgAmMessages );
 		$alldata['deps'][] = new GlobalDependency( 'wgAmMessages' );
-
-		return true;
 	}
 }
